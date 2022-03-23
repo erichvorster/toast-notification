b9560f545bea41170e4db2fe8e24305ce5c70f34
erichvorster <erichvorster1@gmail.com>
2022-03-23 07:23:10 +0200

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 const button = document.getElementById('button');
 const toasts = document.getElementById('toasts');
 
+button.addEventListener('click', () => createNotification())
+
+const messages = [
+    'message 1',
+    'message 2', 
+    'message 3'
+]
+
+function createNotification() {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.innerText = getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    },3000)
+}
+
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)
+    ]
+}
